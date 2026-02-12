392b52db6ad482e298316aaab1606709eca29365
Pratham Dalmia <prathamdalmia2003@gmail.com>
2026-02-12 11:00:22 +0530

    Handled not found api and errorHandler

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./Config/db");
 const colors = require("colors");
 const userRouter = require('./Routes/user.routes');
+const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
 require("dotenv").config()
 
 const app = express();
@@ -12,6 +13,9 @@ app.get("/", (req, res) => {
   res.send("API is Running");
 });
 app.use("/api/user", userRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
