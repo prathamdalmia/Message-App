d04aefae34e036dbde433cecf0ccaf3047afc81c
Pratham Dalmia <prathamdalmia2003@gmail.com>
2026-05-16 16:43:45 +0530

    Real time message

--- frontend/src/Components/singleChat.js
+++ frontend/src/Components/singleChat.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ChatState } from '../Context/chatProvider'
-import { Box, Field, IconButton, Input, Spinner, Text } from '@chakra-ui/react';
+import { Box, Field, IconButton, Input, Spinner, Text, useStatStyles } from '@chakra-ui/react';
 import { getSender } from '../Config/chatLogics';
 import ProfileModal from './Miscellaneous/profileModal'
 import UpdateGroupChatModal from './Miscellaneous/updateGroupChatModal';
@@ -8,12 +8,34 @@ import axios from 'axios';
 import { toaster } from './ui/toaster';
 import './styles.css'
 import ScrollableMessages from './scrollableMessages';
+import io from 'socket.io-client';
+import Lottie from 'react-lottie';
+import animationData from "../Animations/Typing.json";
+
+
+const ENDPOINT = "http://localhost:8000";
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
         const [messages, setMessages] = useState([]);
         const [loading, setLoading] = useState(false);
-        const [newMessage, setNewMessage] = useState("")
+        const [newMessage, setNewMessage] = useState("");
+        const [socketConnected, setSocketConnected] = useState(false);
+        const [typing, setTyping] = useState(false);
+        const [isTyping, setIsTyping] = useState(false)
+
+        const lastTypingTimeRef = useRef(null);
+        const typingRef = useRef(false);
+
+        const defaultOptions = {
+                loop: true,
+                autoplay: true,
+                animationData: animationData,
+                rendererSettings: {
+                        preserveAspectRatio: "xMidYMid slice",
+                },
+        };
 
         const { user, selectedChat, setSelectedChat } = ChatState();
 
@@ -34,6 +56,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
                         setMessages(data);
                         setLoading(false);
+                        socket.emit('join chat', selectedChat._id);
 
                 } catch (error) {
                         toaster.create({
@@ -45,17 +68,43 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 }
         }
 
+        useEffect(() => {
+
+                socket = io(ENDPOINT);
+                socket.emit("setup", user);
+                socket.on('connected', () => setSocketConnected(true));
+                socket.on('typing', () => setIsTyping(true))
+                socket.on('stop typing', () => setIsTyping(false))
+
+        }, []);
+
 
         useEffect(() => {
                 fetchMessages();
                 // console.log(messages);
+                selectedChatCompare = selectedChat;
+
+        }, [selectedChat]);
 
 
-        }, [selectedChat])
+        useEffect(() => {
+                socket.on('message receieved', (newMessageRecieved) => {
+                        if (!selectedChatCompare || selectedChatCompare._id !== newMessageRecieved.chat._id) {
+                                //give notification
+
+                        } else {
+                                setMessages([...messages, newMessageRecieved]);
+                        }
+
+
+                });
+        })
+
 
 
         const sendMessage = async (event) => {
                 if (event.key === "Enter" && newMessage) {
+                        socket.emit('stop typing', selectedChat._id);
                         try {
                                 const config = {
                                         headers: {
@@ -63,13 +112,13 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                                                 Authorization: `Bearer ${user.token}`
                                         }
                                 };
-
+                                setNewMessage("");
                                 const { data } = await axios.post('/api/message', { content: newMessage, chatId: selectedChat._id }, config);
 
                                 // console.log(data);
                                 // console.log(messages);
+                                socket.emit("new message", data)
 
-                                setNewMessage("");
                                 setMessages([...messages, data]);
 
                                 // console.log(messages);
@@ -85,10 +134,39 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                         }
                 }
         }
+
+
+
+
+
+
+
         const typingHandler = (e) => {
                 setNewMessage(e.target.value);
-                // Typing Logic
-        }
+
+                if (!socketConnected) return;
+
+                if (!typingRef.current) {
+                        typingRef.current = true;
+                        setTyping(true);
+                        socket.emit("typing", selectedChat._id);
+                }
+
+                lastTypingTimeRef.current = new Date().getTime();
+
+                const timerLength = 1000;
+
+                setTimeout(() => {
+                        const timeNow = new Date().getTime();
+                        const timeDiff = timeNow - lastTypingTimeRef.current;
+
+                        if (timeDiff >= timerLength && typingRef.current) {
+                                socket.emit("stop typing", selectedChat._id);
+                                typingRef.current = false;
+                                setTyping(false);
+                        }
+                }, timerLength);
+        };
 
 
         return (
@@ -148,6 +226,14 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                                                         )}
 
                                                         <Field.Root onKeyDown={sendMessage} required marginTop={2}>
+                                                                {isTyping ? (
+                                                                        <div>
+                                                                                <Lottie
+                                                                                        options={defaultOptions}
+                                                                                        width={70}
+                                                                                        style={{ marginBottom: 15, marginLeft: 0 }}
+                                                                                />
+                                                                        </div>) : <></>}
                                                                 <Input variant="filled" bg='#d6d5d5' placeholder='Enter a Message...' onChange={typingHandler} value={newMessage} />
                                                         </Field.Root>
 
